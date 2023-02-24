e3751a9c7f7adb5701f81a0ba8df05d6ff986a3e
Islamkg2004 <islam.kg.2004@gmail.com>
2023-02-24 23:07:29 +0600

    dz4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+       password: "",
+      };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+  handlePasswordChange(event) {
+    this.setState({password: event.target.value})
+  }
+  handleSubmit(event) {
+    alert("Ваш имя: " + this.state.value + "Ваш пароль: " + this.state.password);
+    event.preventDefault();
+    this.props.onSubmit(this.state.value);
+    this.setState({value: "", password: ""});
+  }
+
+  render() {
+    return (
+      <div className="nameText">
+         <form onSubmit={this.handleSubmit}>
+          <div className="text">
+          <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+          </div>
+          <div className="password">
+          <input 
+        value={this.state.password}
+        onChange={this.handlePasswordChange}
+        type="password"
+        />
+          </div>
+        <div className="submit">
+        <button  type="submit">Войти</button>
+        </div>
+      </form>
+      </div>
+     
+    );
+  }
 }
 
-export default App;
+export default NameForm;
